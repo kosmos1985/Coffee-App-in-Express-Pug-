ac0542e459597dbd9d32f351e7e968596f7110c8
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-03-28 09:53:25 +0200

    YEEEEEEEEEE.It's work!!!!!!!! Im intall html2pdf and create fite to download

--- public/javascripts/uslugi.js
+++ public/javascripts/uslugi.js
@@ -2,23 +2,8 @@ function generatePDF()
 {
         const text = document.querySelector('textarea').value;
         console.log(text);
-        
-        // const fs = require('fs');
-        // fs.write('text.pdf', text, (err) =>
-        // {
-        //     if (err) throw err;
-        //     console.log('The file has been saved!');
-        // });
+        html2pdf()
+          .from(text)
+          .save();
     
 };
-
-// const puppeteer = require('puppeteer');
-// (async () => {
-//         const browser = await puppeteer.launch();
-//         const page = await browser.newPage();
-//         await page.goto('https://example.com');
-//         await page.pdf({ path: "text.pdf", format: "A4" });
-      
-//         await browser.close();
-//       })();
-
