1e7c091d1815488964f0503404d392a918e2034e
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-03-28 09:12:09 +0200

    I'm trying to find a solution to create pdf file

--- public/javascripts/uslugi.js
+++ public/javascripts/uslugi.js
@@ -1,15 +1,24 @@
-
-const text = document.querySelector('textarea');
-
-function saveText()
+function generatePDF()
 {
-    
+        const text = document.querySelector('textarea').value;
+        console.log(text);
+        
         // const fs = require('fs');
-        // // fs.write('text.pdf', text, (err) =>
-        // // {
-        // //     if (err) throw err;
-        // //     console.log('The file has been saved!');
-        // // });
+        // fs.write('text.pdf', text, (err) =>
+        // {
+        //     if (err) throw err;
+        //     console.log('The file has been saved!');
+        // });
     
 };
+
+// const puppeteer = require('puppeteer');
+// (async () => {
+//         const browser = await puppeteer.launch();
+//         const page = await browser.newPage();
+//         await page.goto('https://example.com');
+//         await page.pdf({ path: "text.pdf", format: "A4" });
+      
+//         await browser.close();
+//       })();
 
